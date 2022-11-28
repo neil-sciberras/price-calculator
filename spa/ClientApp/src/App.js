d4598e8196fbf5efb19a96c12515e7ad5b474743
Neil Sciberras <neilxib@gmail.com>
2022-11-28 20:32:28 +0100

    Deleted FetchData component

--- spa/ClientApp/src/App.js
+++ spa/ClientApp/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import { FetchPrice } from './components/FetchPrice';
 import { Counter } from './components/Counter';
 
@@ -16,7 +15,6 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/fetch-price' component={FetchPrice} />
       </Layout>
     );
--- spa/ClientApp/src/components/NavMenu.js
+++ spa/ClientApp/src/components/NavMenu.js
@@ -37,9 +37,6 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-                <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-price">Fetch price</NavLink>
                 </NavItem>
               </ul>
