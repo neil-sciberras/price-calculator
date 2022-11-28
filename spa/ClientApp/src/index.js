3973504d19ad137411bb3e1e02f94b619b594e5a
Neil Sciberras <neilxib@gmail.com>
2022-11-28 22:13:30 +0100

    Deleted unnecessary files

--- spa/ClientApp/src/index.js
+++ spa/ClientApp/src/index.js
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
