a5c227d6683e48e213a979dcb5a671fce89cf75c
Neil Sciberras <neilxib@gmail.com>
2022-11-29 20:13:19 +0100

    Setting env variables and url

--- spa/ClientApp/src/setupProxy.js
+++ spa/ClientApp/src/setupProxy.js
@@ -1,8 +1,11 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:40004';
+const target = env.ASPNETCORE_HTTPS_PORT
+    ? `https://localhost:443`
+    : env.ASPNETCORE_URLS
+        ? env.ASPNETCORE_URLS.split(';')[0]
+		: 'http://localhost:40004';
 
 const context =  [
   "/pricecalculator",
