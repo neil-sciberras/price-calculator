4db276e4b17710be72802717254fbc29461022ec
Neil Sciberras <neilxib@gmail.com>
2022-11-29 17:50:52 +0100

    Fixes

--- spa/ClientApp/src/App.js
+++ spa/ClientApp/src/App.js
@@ -2,91 +2,85 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [weight, setWeight] = useState("");
-    const [height, setHeight] = useState("");
-    const [width, setWidth] = useState("");
-    const [depth, setDepth] = useState("");
-    const [statusCode, setStatusCode] = useState("");
-    const [price, setPrice] = useState("");
-    const [message, setMessage] = useState("");
+	const [weight, setWeight] = useState("");
+	const [height, setHeight] = useState("");
+	const [width, setWidth] = useState("");
+	const [depth, setDepth] = useState("");
+	const [message, setMessage] = useState("");
 
-    let handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await fetch(
-                'https://localhost:7181/Price',
-                {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        weight: weight,
-                        dimensions: {
-                            width: width,
-                            height: height,
-                            depth: depth
-                        }
-                    })
-                })
-                .then(response =>
-                {
-                    setStatusCode(response.status);
-                    return response.json();
-                })
-                .then(data => setPrice(data));
+	let handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			let response = await fetch('https://localhost:7181/Price',
+				{
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						weight: weight,
+						dimensions: {
+							width: width,
+							height: height,
+							depth: depth
+						}
+					})
+				});
+			
+			if (response.status === 200) {
+				let price = await response.json();
 
-            if (statusCode === 200)
-            {
-                setWeight("");
-                setHeight("");
-                setWidth("");
-                setDepth("");
-                setMessage("Request sent successfull. Price is: " + price);
-            }
-            else
-            {
-                setMessage("Some error occured. Status code is:" + statusCode);
-            }
-        }
-        catch (err)
-        {
-            console.log(err);
-        }
-    };
+				setMessage("Price is: " + price);
+			}
+			else {
+				let errorMessage = await response.text();
 
-    return (
-        <div className="App">
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="number"
-                    value={weight}
-                    placeholder="Weight"
-                    onChange={(e) => setWeight(e.target.value)}
-                />
-                <input
-                    type="number"
-                    value={height}
-                    placeholder="Height"
-                    onChange={(e) => setHeight(e.target.value)}
-                />
-                <input
-                    type="number"
-                    value={width}
-                    placeholder="Width"
-                    onChange={(e) => setWidth(e.target.value)}
-                />
-                <input
-                    type="number"
-                    value={depth}
-                    placeholder="Depth"
-                    onChange={(e) => setDepth(e.target.value)}
-                />
+				setMessage("Something went wrong. (" +  errorMessage + ")");
+			}
 
-                <button type="submit">Calculate</button>
+			setWeight("");
+			setHeight("");
+			setWidth("");
+			setDepth("");
+		}
+		catch (err)
+		{
+			console.log(err);
+		}
+	};
 
-                <div className="message">{message ? <p>{message}</p> : null}</div>
-            </form>
-        </div>
-    );
+	return (
+		<div className="App">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="number"
+					value={weight}
+					placeholder="Weight"
+					onChange={(e) => setWeight(e.target.value)}
+				required />
+				<input
+					type="number"
+					value={height}
+					placeholder="Height"
+					onChange={(e) => setHeight(e.target.value)}
+				required />
+				<input
+					type="number"
+					value={width}
+					placeholder="Width"
+					onChange={(e) => setWidth(e.target.value)}
+				required />
+				<input
+					type="number"
+					value={depth}
+					placeholder="Depth"
+					onChange={(e) => setDepth(e.target.value)}
+				required />
+
+				<button type="submit">Calculate</button>
+
+				<div className="message">{message ? <p>{message}</p> : null}</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
--- spa/ClientApp/src/setupProxy.js
+++ spa/ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:40004';
 
 const context =  [
-  "/weatherforecast",
+  "/pricecalculator",
 ];
 
 module.exports = function(app) {
